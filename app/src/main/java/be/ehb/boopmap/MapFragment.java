25fdbde8d7312d02e8376c8344d937838ce31020
TijlDeRidder <80417938+TijlDeRidder@users.noreply.github.com>
2023-01-06 13:27:16 +0100

    Added map
    MapFragment using SupportMapFragment
    
    added a LatLng for Brussel

--- app/src/main/java/be/ehb/boopmap/MapFragment.java
+++ app/src/main/java/be/ehb/boopmap/MapFragment.java
@@ -9,11 +9,19 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-import be.ehb.boopmap.databinding.FragmentSecondBinding;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+import be.ehb.boopmap.databinding.FragmentMapBinding;
+
 
 public class MapFragment extends Fragment {
-
-    private FragmentSecondBinding binding;
+    private GoogleMap map;
+    private SupportMapFragment supportMapFragment;
+    private FragmentMapBinding binding;
 
     @Override
     public View onCreateView(
@@ -21,19 +29,26 @@ public class MapFragment extends Fragment {
             Bundle savedInstanceState
     ) {
 
-        binding = FragmentSecondBinding.inflate(inflater, container, false);
+        binding = FragmentMapBinding.inflate(inflater, container, false);
         return binding.getRoot();
 
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-        binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
+        supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull GoogleMap googleMap) {
+                LatLng BRUSSEL = new LatLng(50.84402768761813, 4.35406770435145);
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(BRUSSEL, 15));
+            }
+        });
+        binding.btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 NavHostFragment.findNavController(MapFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
+                        .navigate(R.id.action_mapFragment_to_FirstFragment);
             }
         });
     }
